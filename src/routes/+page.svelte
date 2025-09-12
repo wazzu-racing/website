8315402a99c003c23a309b860db9d524d0ab4ea6
Calvin Tallent <calvintallentspam@gmail.com>
2025-09-12 14:46:04 -0700

    Add basic message to main page to prepare for initial deployment.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Welcome to Wazzu Racing!</h1>
+
+<p>We're still working on our website, it should be complete soon!</p>
+
+<p>
+	We meet every Saturday at 11 AM in <a href="https://maps.app.goo.gl/15P6iwDgqBRDkxYw6">ELB 9</a>.
+	Come see what we're up to!
+</p>
