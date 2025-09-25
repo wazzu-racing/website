6d25f40ce388f53e12f74cdbfb0f17b953f926d0
Calvin Tallent <calvintallentspam@gmail.com>
2025-09-24 17:53:10 -0700

    Fix unneeded closing div tag.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,87 +18,81 @@
 	config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 </script>
 
-
-
 <div class="bod">
-
-	<div class="info"> 
-	<!-- The video -->
+	<div class="info">
+		<!-- The video -->
 		<video autoplay muted loop>
-			<source src="src/lib/assets/carSlideshow.mp4" type="video/mp4">
+			<source src="src/lib/assets/carSlideshow.mp4" type="video/mp4" />
 		</video>
 
 		<div class="content">
 			<h1>Washington State University</h1>
 			<h1>Wazzu Racing</h1>
-			<p>Washington State Univiersity Formuala SAE | Cars Designed Built and Tested by Student Engineers</p>
+			<p>
+				Washington State Univiersity Formuala SAE | Cars Designed Built and Tested by Student
+				Engineers
+			</p>
 		</div>
-
 	</div>
 
 	<!-- could delete this section -->
 	<div class="meetInfo">
 		<div class="Join">
 			<h1>Join Us</h1>
-			<h2>Formula SAE at Washington State University. Formula-style racing car conceived, designed, and fabriacted by students.</h2>
+			<h2>
+				Formula SAE at Washington State University. Formula-style racing car conceived, designed,
+				and fabriacted by students.
+			</h2>
 			<p>Meet us at ELB 9 every Saturday at 11AM</p>
-
 		</div>
 
-			<div class="Collumn">
-				<img src="src/lib/assets/One.jpeg" alt=" ">
-				<img src="src/lib/assets/Five.jpeg" alt=" ">
-				<img src="src/lib/assets/Nine.jpeg" alt=" ">
-				<img src="src/lib/assets/Twelve.jpeg" alt=" ">
-			</div>
+		<div class="Collumn">
+			<img src="src/lib/assets/One.jpeg" alt=" " />
+			<img src="src/lib/assets/Five.jpeg" alt=" " />
+			<img src="src/lib/assets/Nine.jpeg" alt=" " />
+			<img src="src/lib/assets/Twelve.jpeg" alt=" " />
+		</div>
 
-			<div class="Collumn">
-				<img src="src/lib/assets/Two.jpeg" alt=" ">
-				<img src="src/lib/assets/Six.jpeg" alt=" ">
-				<img src="src/lib/assets/Ten.png" alt=" ">
-				<img src="src/lib/assets/Eight.png" alt=" ">
-			</div>
+		<div class="Collumn">
+			<img src="src/lib/assets/Two.jpeg" alt=" " />
+			<img src="src/lib/assets/Six.jpeg" alt=" " />
+			<img src="src/lib/assets/Ten.png" alt=" " />
+			<img src="src/lib/assets/Eight.png" alt=" " />
+		</div>
 
-			<div class="Collumn">
-				<img src="src/lib/assets/Three.jpeg" alt=" ">
-				<img src="src/lib/assets/Seven.jpeg" alt=" ">
-				<img src="src/lib/assets/Eleven.png" alt=" ">
-				<img src="src/lib/assets/Four.png" alt=" ">
-			</div>
-
-
-
+		<div class="Collumn">
+			<img src="src/lib/assets/Three.jpeg" alt=" " />
+			<img src="src/lib/assets/Seven.jpeg" alt=" " />
+			<img src="src/lib/assets/Eleven.png" alt=" " />
+			<img src="src/lib/assets/Four.png" alt=" " />
 		</div>
 	</div>
-	<!-- could delete this section -->
+</div>
+<!-- could delete this section -->
 
-	<div class="contact">
-		<h1>FOLLOW US</h1>
-		<ul>
-			<li>
-				<a href="https://www.instagram.com/wazzu_racing/">
-					<FontAwesomeIcon icon={faSquareInstagram} />
-				</a>
-			</li>
-			<li>
-				<a href="https://x.com/wazzuracing"><FontAwesomeIcon icon={faSquareTwitter} /> </a>
-			</li>
-			<li>
-				<a href="mailto:someone@example.com"><FontAwesomeIcon icon={faSquareEnvelope} /> </a>
-			</li>
-			<li>
-				<a href="https://www.linkedin.com/company/wazzu-racing/"
-					><FontAwesomeIcon icon={faSquareLinkedin} />
-				</a>
-			</li>
-		</ul>
-	</div>
+<div class="contact">
+	<h1>FOLLOW US</h1>
+	<ul>
+		<li>
+			<a href="https://www.instagram.com/wazzu_racing/">
+				<FontAwesomeIcon icon={faSquareInstagram} />
+			</a>
+		</li>
+		<li>
+			<a href="https://x.com/wazzuracing"><FontAwesomeIcon icon={faSquareTwitter} /> </a>
+		</li>
+		<li>
+			<a href="mailto:someone@example.com"><FontAwesomeIcon icon={faSquareEnvelope} /> </a>
+		</li>
+		<li>
+			<a href="https://www.linkedin.com/company/wazzu-racing/"
+				><FontAwesomeIcon icon={faSquareLinkedin} />
+			</a>
+		</li>
+	</ul>
 </div>
 
 <style>
-
-
-
 	.bod {
 		display: flex;
 		flex-direction: column;
@@ -112,12 +106,12 @@
 	.info video {
 		min-width: 100%;
 		min-height: 100%;
-	} 
+	}
 
 	.content {
 		position: absolute;
-		width: 100%
-		
+		width: 100%;
+
 		/* color: var(--secondary-white) */
 	}
 
@@ -135,28 +129,22 @@
 		font-size: 1.5em;
 		color: var(--secondary-white);
 		text-align: center;
-
 	}
 
-	
-
 	.meetInfo {
-
 		display: flex;
 		width: 100%;
-
 	}
 
 	.Join {
 		/* background-color: black; */
 		display: flex;
 		flex-direction: column;
-		width:30%;
+		width: 30%;
 		padding-top: 5px;
 		padding-right: 24px;
 		padding-bottom: 24px;
 		padding-left: 24px;
-
 	}
 
 	.Join h1 {
@@ -187,16 +175,15 @@
 		display: flex;
 		flex-direction: column;
 		flex-wrap: wrap;
-		width:70%;
+		width: 70%;
 	}
 
 	.Collumn {
 		display: flex;
 	}
 
-	.Collumn img{
+	.Collumn img {
 		width: 22%;
-	
 
 		margin-top: 8px;
 		margin-right: 8px;
@@ -208,7 +195,6 @@
 	/**----------------------------------*/
 
 	.contact {
-
 		align-content: end;
 		padding-top: 15px;
 		padding-bottom: 15px;
